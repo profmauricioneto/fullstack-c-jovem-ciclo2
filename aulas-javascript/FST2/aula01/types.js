f8d01403314bbd3ce252bc762dfb6d3825868ab6
profmauricioneto <mauricio.moreira@unichristus.edu.br>
2025-05-20 15:27:49 -0300

    add: primeira aula fst2 javascript

---
+++ aulas-javascript/FST2/aula01/types.js
@@ -0,0 +1,8 @@
+let nome = 'mauricio';
+
+console.log(typeof nome);
+console.log(typeof 35);
+console.log(typeof 1.8);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
