0eeb6753b01e3ae595b06458fa55e977bfae006e
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2025-08-14 15:27:57 -0300

    logger com pino fst2

--- aulas-node/fst2/src/exercicio04/modules/auth/user.services.js
+++ aulas-node/fst2/src/exercicio04/modules/auth/user.services.js
@@ -2,28 +2,42 @@ require("dotenv").config();
 const usersData = require("../../config/data.users");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const logger = require('../../shared/logger/logger');
+
 let idUser = 2;
 
 const JWT_SECRET = process.env.JWT_SECRET_KEY;
+
+if (!JWT_SECRET) {
+  logger.error("Chave JWT SECRET não foi carregada corretamente");
+  throw new Error("JWT SECRET não carregada!");
+}
 
 const registerUser = async (nome, email, senha) => {
   try {
     // verificação de entrada de dados
     if (!nome || !email || !senha) {
-      console.error(`Campos obrigatórios não preenchidos`);
-      return;
+      logger.error("nome, email e senha estão incompletos.");
+      throw new Error("nome, email e senha são obrigatórias");
+      // console.error(`Campos obrigatórios não preenchidos`);
+      // return;
     }
     if (senha.length < 6) {
-      console.error(`A senha deve conter no mínimo 6 digitos`);
-      return;
+      logger.error("senha deve ter o mínimo de 6 digitos.");
+      throw new Error("senha deve ter o mínimo de 6 digitos.");
+      // console.error(`A senha deve conter no mínimo 6 digitos`);
+      // return;
     }
 
     // verificar se o usuário já possui cadastro
     const userExists = usersData.find((user) => user.email === email);
     if (userExists) {
-      console.log(`Usuário já cadastrado na plataforma.`);
+      // console.log(`Usuário já cadastrado na plataforma.`);
+      logger.warn("Usuário já existente no sistema");
       return userExists;
     }
+
+    logger.info("Início do cadastro do usuário: ", {nome, email});
 
     // criptografando a senha
     const cryptPassword = await bcrypt.hash(senha, 10);
@@ -46,9 +60,19 @@ const registerUser = async (nome, email, senha) => {
       { expiresIn: "10min" }
     );
 
+    logger.info("Usuário cadastrado com sucesso ", {
+      userId: newUser.id,
+      userEmail: newUser.email,
+    });
+
     return { newUser, token };
   } catch (error) {
-    console.error(`Não foi possível realizar o login`);
+    // console.error(`Não foi possível realizar o login`);
+    logger.error("Erro ao cadastrar o usuário", {
+      error: error.message,
+      email,
+    });
+    throw error
   }
 };
 
@@ -56,28 +80,38 @@ const getAllUsers = () => {
   try {
     return usersData;
   } catch (error) {
-    console.error(`Erro ao retorna os usuários.`);
+    // console.error(`Erro ao retorna os usuários.`);
+      logger.error("Erro ao acessar os usuários", {
+      error: error.message,
+    });
+    throw error
   }
 };
 
 const login = async (email, senha) => {
   try {
     if (!email || !senha) {
-      console.error(`Campos obrigatórios estão incompletos.`);
-      return;
+      logger.error("email e senha estão incompletos.");
+      throw new Error("email e senha são obrigatórias");
+      // console.error(`Campos obrigatórios estão incompletos.`);
+      // return;
     }
 
     const userExists = usersData.find((user) => user.email === email);
     if (!userExists) {
-      console.error(`Usuário não cadastro na aplicação.`);
-      return;
+      // console.error(`Usuário não cadastro na aplicação.`);
+      // return;
+      logger.warn("Usuário não cadastrado na aplicação.");
+      throw new Error("Usuário não cadastrado na aplicação.");
     }
 
     // verficação de senha!
     const passwordValid = await bcrypt.compare(senha, userExists.senha);
     if (!passwordValid) {
-      console.error(`Senha incorreta!`);
-      return;
+      logger.error("Senha inválida.");
+      throw new Error("Senha inválida.");
+      // console.error(`Senha incorreta!`);
+      // return;
     }
 
     // gerando a token jwt
@@ -87,9 +121,18 @@ const login = async (email, senha) => {
       { expiresIn: "10min" }
     );
 
+    logger.info('Login realizado com sucesso: ', {
+      userId: userExists.id,
+      email
+    });
+
     return { userExists, token }
   } catch (error) {
-    console.error(`Erro ao fazer o login na aplicação.`);
+      logger.error("Erro ao realizad o login", {
+      error: error.message,
+      email,
+    });
+    throw error
   }
 };
 
--- aulas-node/fst2/src/exercicio04/server.js
+++ aulas-node/fst2/src/exercicio04/server.js
@@ -1,14 +1,24 @@
+require('dotenv').config();
+const logger = require('./shared/logger/logger');
+const loggerHTTP = require('./shared/middlewares/loggerMiddleware');
 const express = require('express');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
+app.use(loggerHTTP);
 app.use(express.json());
 
 const routerProduct = require('./modules/product/product.routes');
 const routerUsers = require('./modules/auth/user.routes');
 
 app.use('/api', routerProduct);
-app.use('/api/auth', routerUsers)
+app.use('/api/auth', routerUsers);
+
+logger.info("Start Application", {
+    environment: process.env.NODE_ENV,
+    port: PORT
+});
 
 app.listen(PORT,() => {
     console.log(`Server running in http://localhost:${PORT}`);
