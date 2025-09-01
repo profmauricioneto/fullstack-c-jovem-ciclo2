89eede65d606da3b2e26b308a11772c04c814f94
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2025-09-01 15:22:23 -0300

    aula integração entre sistemas fst1 - parte 1

--- aulas-node/fst1/src/exercicio05/server.js
+++ aulas-node/fst1/src/exercicio05/server.js
@@ -1,10 +1,35 @@
 const logger = require('./shared/logger/logger');
 const httpLogger = require('./shared/middlewares/middlewareLogger')
+const cors = require('cors');
 const express = require('express');
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+const corsOptions = {
+    origin: function (origin, callback) {
+        const allowedResource = ['http://localhost:5173', 'http://localhost:3000'];
+        if (!origin) return callback(null, true);
+        if (allowedResource.indexOf(origin) !== -1) {
+            return callback(null, true);
+        } else {
+            logger.warn('CORS blocked this request: ', {origin});
+            callback(new Error('Não permitido pelo CORS.'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: [
+        'Content-type',
+        'Authorization',
+        'X-Correlation-ID',
+        'Origin',
+        'Accept',
+    ],
+    exposedHeaders: ['X-Correlation-ID'],
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(httpLogger);
 
